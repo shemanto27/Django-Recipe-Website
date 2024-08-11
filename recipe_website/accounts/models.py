7d16d18599315e78911a4d5e5d752d1185ecf78d
shemanto27 <shemantosharkarofficial@gmail.com>
2024-08-11 17:44:32 +0600

    2nd day

--- recipe_website/accounts/models.py
+++ recipe_website/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class accounts(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    userName = models.CharField(max_length=20)
+    userEmail = models.EmailField()
+    phone = models.IntegerField(null=True)
+    userGender = models.CharField(max_length=6,null=True)
+    joinedDate = models.DateField(null=True)
--- recipe_website/accounts/views.py
+++ recipe_website/accounts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def registration(request):
-    return render(request, "registration.html", context={'page_title' : 'Registration Page',})
+    if request.method == 'POST':
+        data = request.POST
+        user_name = data.get('userName')
+        user_email = data.get('userEmail/')
+        phone_number = data.get('phone')
+        gender = data.get('userGender')
+        
+
+    context={'page_title' : 'Registration Page',}
+    return render(request, "registration.html", context)
 
 def login(request):
-    return render(request, "login.html", context={'page_title':'Registration Page',})
+    context={'page_title':'Registration Page',}
+    return render(request, "login.html", context)
